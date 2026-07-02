63b8fefa342718f2645178714e6975145f009212
noelberistain <noelberistain@gmail.com>
2026-07-02 14:39:00 -0700

    initialize Db

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from 'expo-router';
 import '../global.css';
 
+
+import { initDb } from "@/db/initDb";
+
+initDb();
+
+
 export default function RootLayout() {
 	return (
 		<Stack
