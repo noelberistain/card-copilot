0c619f9d098cf830a74cc10f88e01a9d598dd2de
noelberistain <noelberistain@gmail.com>
2026-07-02 14:37:29 -0700

    Update client.ts

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,6 +2,6 @@ import { drizzle } from 'drizzle-orm/expo-sqlite';
 import { openDatabaseSync } from 'expo-sqlite';
 import * as schema from './schema';
 
-const sqlite = openDatabaseSync('card-copilot.db');
+export const sqlite = openDatabaseSync('card-copilot.db');
 
 export const db = drizzle(sqlite, { schema });
