d86582ec631e10abdf369aaf211b99a6902bc946
noelberistain <noelberistain@gmail.com>
2026-07-02 14:40:09 -0700

    fix SafeAreaView warning

--- src/components/ui/ScreenContainer.tsx
+++ src/components/ui/ScreenContainer.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 interface ScreenContainerProps {
 	children: ReactNode;
